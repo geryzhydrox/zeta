70f9fd0189c5295165fd44130f1204259310b98a
gerald <geraldtrautsch@gmail.com>
2025-06-13 00:25:19 +0200

    Made `zeta add` add to the `root.scm` file.

--- zeta/system.scm
+++ zeta/system.scm
@@ -4,19 +4,26 @@
   #:use-module (ice-9 match)
   #:use-module (zeta term)
   #:export (%zeta-root
+	    %root-manifest
 	    relative->absolute
 	    mkdir-p
 	    touch
 	    make-file-at-path
 	    make-scm-file-at-path
+	    read-file
 	    write-file
 	    read-pkgs
-	    read-manifests))
+	    read-manifests
+	    manifest-with-pkgs
+	    root-with-manifests))
 
 
 (define %zeta-root (or (getenv "ZETA_ROOT")
 		       (string-append (getenv "HOME") "/.zeta")
 		       (error-with-msg "Something went seriously wrong. $HOME environment variable cannot be read.")))
+
+(define %root-manifest
+  (string-append %zeta-root "/root.scm"))
 
 (define (relative->absolute rel-path)
   (string-append %zeta-root
@@ -61,5 +68,28 @@
 	       ('call-with-input-file 'filepath
 		 ('lambda ('input-port)
 		   ('read 'input-port)))))
-	    ('quote (pkgs ...)))) pkgs)
+	    ('quote (manifests ...)))) manifests)
     (_ '())))
+
+(define (manifest-with-pkgs pkgs)
+  (format #f "(specifications->manifest\n '(~a))"
+	  (string-join
+	   (map (lambda (pkg)
+		  (format #f "\"~a\"" pkg))
+		pkgs)
+	   "\n   ")))
+
+(define (root-with-manifests manifests)
+  (format #f
+"(concatenate-manifests
+  (map (lambda (filepath)
+     (primitive-eval
+       (call-with-input-file filepath
+         (lambda (input-port)
+           (read input-port)))))
+  '(~a)))"
+	  (string-join
+	   (map (lambda (manifest)
+		  (format #f "\"~a\"" manifest))
+		manifests)
+	   "\n    ")))
