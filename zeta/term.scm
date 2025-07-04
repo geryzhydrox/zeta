84d2890b99f77fa5328b5a358e0a93161915e91d
gerald <geraldtrautsch@gmail.com>
2025-07-04 13:03:00 +0200

    Made code for (zeta term) more readable. No more "magic constants".

--- zeta/term.scm
+++ zeta/term.scm
@@ -3,8 +3,7 @@
   #:export (println
 	    usage
 	    error-with-msg
-	    info-with-msg
-	    ))
+	    info-with-msg))
 
 (define (println str)
   (display str)
@@ -14,17 +13,26 @@
   (println "Usage: zeta [install|remove|add|del] [manifest] [packages]")
   (exit 1))
 
+(define (colorize str color)
+  (define terminate "\x1b[0m")
+  (define colors
+    '((red . "\x1b[1;31m")
+      (blue . "\x1b[1;34m")
+      (yellow . "\x1b[1;33m")))
+  (format #f "~a~a~a"
+	  (or (assoc-ref colors color) (error "Color not defined."))
+	  str
+	  terminate))
+
 (define (error-with-msg msg)
-  (display "\x1b[1;31mERROR: \x1b[0m")
+  (display (colorize "ERROR: " 'red))
   (println msg)
   (exit 1))
 
 (define (info-with-msg msg)
-  (display "\x1b[1;34mINFO: \x1b[0m")
+  (display (colorize "INFO: " 'blue))
   (println msg))
 
 (define (warning-with-msg msg)
-  (display "\x1b[1;33mWARNING: \x1b[0m")
+  (display (colorize "WARNING: " 'yellow))
   (println msg))
-
-
